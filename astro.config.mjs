4c2c8ec8197a6827fe21b954d7b31208a94b5e09
Felix <felix@hinterstellar.io>
2024-06-23 16:53:41 +0200

    [front] Get ready for launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,39 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Bmg - Relational Algebra for Ruby',
-			social: {
-				github: 'https://github.com/enspirit/bmg',
-			},
-			sidebar: [
-				{
-					label: 'Relational Algebra Primer',
-					autogenerate: { directory: 'ra-primer' },
-				},
-				{
-					label: 'Usage',
-					autogenerate: { directory: 'usage' },
-				},
-				{
-					label: 'Recipies',
-					autogenerate: { directory: 'recipies' },
-				},
-				{
-					label: 'Operations reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			expressiveCode: {
-				themes: ['dracula']
-			}
-		}),
-	],
+  integrations: [starlight({
+    title: 'Bmg - Relational Algebra for Ruby',
+    favicon: '/favicon.ico',
+    social: {
+      github: 'https://github.com/enspirit/bmg'
+    },
+    sidebar: [{
+      label: 'Relational Algebra Primer',
+      autogenerate: {
+        directory: 'ra-primer'
+      }
+    }, {
+      label: 'Usage',
+      autogenerate: {
+        directory: 'usage'
+      }
+    }, {
+      label: 'Recipies',
+      autogenerate: {
+        directory: 'recipies'
+      }
+    }, {
+      label: 'Operations reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }],
+    expressiveCode: {
+      themes: ['dracula']
+    }
+  }), tailwind()]
 });
