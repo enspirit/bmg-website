f915c5ed1ff54e15360a627edc325b0bcf035852
Bernard Lambeau <bernard.lambeau@enspirit.be>
2024-06-25 18:01:43 +0200

    Add simpleanalytics tracking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,20 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
+    head: [
+      {
+        tag: 'script',
+        attrs: {
+          src: 'https://scripts.simpleanalyticscdn.com/latest.js',
+          defer: true,
+          async: true,
+        },
+      },
+      {
+        tag: 'noscript',
+        content: '<img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" />',
+      },
+    ],
     title: 'Bmg - Relational Algebra for Ruby',
     favicon: '/favicon.ico',
     customCss: [
